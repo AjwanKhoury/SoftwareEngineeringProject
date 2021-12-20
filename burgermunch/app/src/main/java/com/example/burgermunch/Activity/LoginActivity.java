dba2564eb6f4893ed0a7c71bc0faa49c1af279e3
koren508 <koren508@madaimas.org.il>
2021-12-20 13:11:24 +0200

    facebook login

--- burgermunch/app/src/main/java/com/example/burgermunch/Activity/LoginActivity.java
+++ burgermunch/app/src/main/java/com/example/burgermunch/Activity/LoginActivity.java
@@ -1,35 +1,68 @@
 package com.example.burgermunch.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.burgermunch.R;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        fAuth = FirebaseAuth.getInstance();
+        LoginButton FacebookBtn=findViewById(R.id.FacebookBtn);
+        CallbackManager callbackManager = CallbackManager.Factory.create();
+        FacebookBtn.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
 
-        Button FacebookBtn=findViewById(R.id.FacebookBtn);
-        Button GoogleBtn=findViewById(R.id.GoogleBtn);
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(@NonNull FacebookException e) {
+
+            }
+        });
         ImageView loginBtn=findViewById(R.id.loginBtn);
         TextView register_txt=findViewById(R.id.register_txt);
         final EditText email_txt=findViewById(R.id.email_txt);
         final EditText password_txt=findViewById(R.id.password_txt);
 
+
+
         register_txt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
+            }
+        });
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
             }
         });
     }
