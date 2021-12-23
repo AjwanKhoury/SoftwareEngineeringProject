bfa6e8b69e8d4ce78ecf5723a2e3256fc9d49713
Liroy <zaqwedsxc2@gmail.com>
2021-12-23 20:24:54 +0200

    LastChanges

--- burgermunch/app/src/main/java/com/example/burgermunch/Activity/MainActivity.java
+++ burgermunch/app/src/main/java/com/example/burgermunch/Activity/MainActivity.java
@@ -7,7 +7,6 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
