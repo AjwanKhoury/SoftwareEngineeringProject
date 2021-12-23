3503622d6597350b8e9b92f498bdfd6f4a57d4b7
Liroy <zaqwedsxc2@gmail.com>
2021-12-23 18:36:24 +0200

    Order

---
+++ burgermunch/app/src/main/java/com/example/burgermunch/Object/Order.java
@@ -0,0 +1,57 @@
+package com.example.burgermunch.Object;
+
+import java.util.HashMap;
+
+public class Order implements IOrder{
+    private String Date;
+    private String Status;
+    private String CusPhone;
+    private String OrderID;
+
+    public Order(){
+        Date ="";
+        Status ="";
+        CusPhone ="";
+        OrderID ="";
+    }
+
+    public Order(String Da,String St, String Cus, String Or){
+        this.Date= Da;
+        this.Status= St;
+        this.CusPhone= Cus;
+        this.OrderID= Or;
+    }
+
+    @Override
+    public String getDate() {
+        return Date;
+    }
+    public void setDate(String Sd){
+        this.Date=Sd;
+    }
+
+    @Override
+    public String getStatus() {
+        return Status;
+    }
+    public void setStatus(String status) {
+        this.Status = status;
+    }
+
+    @Override
+    public String getOrderID() {
+        return OrderID;
+    }
+    public void setOrderID(String orderID) {
+        this.OrderID = orderID;
+    }
+
+    @Override
+    public String getCusPhone() {
+        return CusPhone;
+    }
+    public void setCusPhone(String cusPhone) {
+        this.CusPhone = cusPhone;
+    }
+
+}
