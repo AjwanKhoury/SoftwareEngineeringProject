3322cc7505093a992859677f23cbfa3123dd588e
Liroy <zaqwedsxc2@gmail.com>
2022-01-06 19:18:09 +0200

    LogOut

--- burgermunch/app/src/main/java/com/example/burgermunch/Activity/CustomerPageActivity.java
+++ burgermunch/app/src/main/java/com/example/burgermunch/Activity/CustomerPageActivity.java
@@ -2,15 +2,29 @@ package com.example.burgermunch.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import com.example.burgermunch.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
 
 public class CustomerPageActivity extends AppCompatActivity {
+    private LinearLayout LogOutBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_page);
+        LogOutBtn = findViewById(R.id.LogOutBtn);
+        LogOutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(CustomerPageActivity.this, MainActivity.class));
+            }
+        });
     }
 }
